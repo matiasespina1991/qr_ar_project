38cc9a0b13a3dfc2d5c14796ef6655a11e92ead3
devbite <matiasespina1991@gmail.com>
2023-08-04 13:41:14 +0200

    stable channel - app working

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
-module.exports = {};
+module.exports = {
+    distDir: 'out',
+    // images: {
+    //   loader: 'akamai',
+    //   path: '',
+    // },
+    // exportPathMap: async function () {
+    //   return {
+    //     '/': { page: '/' }, // Example mapping for the root page
+    //     '/about': { page: '/about' }, // Example mapping for the about page
+    //     // Add mappings for other pages as needed
+    //   };
+    // },
+  }
--- src/layouts/sidebars/vertical/Sidebar.js
+++ src/layouts/sidebars/vertical/Sidebar.js
@@ -71,7 +71,11 @@ const Sidebar = ({ showMobilemenu }) => {
           onClick={showMobilemenu}
         ></Button> */}
       </div>
-      <div className="pt-4 mt-2">
+      
+
+
+
+      {/* <div className="pt-4 mt-2">
         <Nav vertical className="sidebarNav">
           {navigation.map((navi, index) => (
             <NavItem key={index} className="sidenav-bg">
@@ -91,7 +95,27 @@ const Sidebar = ({ showMobilemenu }) => {
           ))}
     
         </Nav>
-      </div>
+      </div> */}
+
+<div className="pt-4 mt-2">
+  <Nav vertical className="sidebarNav">
+    {navigation.map((navi, index) => (
+      <NavItem key={index} className="sidenav-bg">
+        <Link href={navi.href} className={
+              location === navi.href
+                ? "text-primary nav-link py-3"
+                : "nav-link text-secondary py-3"
+            }>
+          <i className={navi.icon}></i>
+          <span className="ms-3 d-inline-block">{navi.title}</span>
+        </Link>
+      </NavItem>
+    ))}
+  </Nav>
+</div>
+
+
+
     </div>
   );
 };
